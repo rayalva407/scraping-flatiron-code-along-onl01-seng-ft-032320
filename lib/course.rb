135b44cf1b237f2a6c63eead9038d7727bf3999c
Ray Alva <rayalva407@gmail.com>
2020-04-10 18:54:38 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,20 @@
 class Course
+  
   attr_accessor :title, :schedule, :description
   
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
   
 end
 
